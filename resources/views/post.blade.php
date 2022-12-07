3ddc1b9a8addcc81cc47cec4feab4b0dd796c425
ariefcu <ariefcu@gmail.com>
2022-12-08 06:21:35 +0700

    akhir dari Belajar Laravel 8 : #8 Post Category & Eloquent Relationship

--- resources/views/post.blade.php
+++ resources/views/post.blade.php
@@ -4,6 +4,7 @@
 
 @section('container')
     <h1 class="mb-5">{{ $post->title }} </h1>    
+    <p>By. Arief Bin M. Sri Narto in <a href="/categories/{{ $post->category->slug }}">{{ $post->category->name }}</a></p>
     {!! $post->body !!}
     <a href="/posts">Back to Blog</a>
 @endsection
