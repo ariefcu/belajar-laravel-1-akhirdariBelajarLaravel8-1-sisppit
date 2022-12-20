2b32f5dad3e43f43a327d3215bdcc4465ea38143
ariefcu <ariefcu@gmail.com>
2022-12-20 10:09:27 +0700

    akhir dari Belajar Laravel 8 : #23 Authorization

--- app/Providers/AppServiceProvider.php
+++ app/Providers/AppServiceProvider.php
@@ -3,7 +3,9 @@
 namespace App\Providers;
 
 use Illuminate\Support\ServiceProvider;
+use Illuminate\Support\Facades\Gate;
 use Illuminate\Pagination\Paginator;
+use App\Models\User;
 
 class AppServiceProvider extends ServiceProvider
 {
@@ -27,5 +29,9 @@ class AppServiceProvider extends ServiceProvider
     {
         Paginator::useBootstrapFive();
         Paginator::useBootstrapFour();
+
+        Gate::define('admin', function(User $user) {
+            return $user->is_admin;
+        });
     }
 }
